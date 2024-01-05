e948b687f2660a6976e00936067792070e49d09f
MuhammadMujtabaRaza <mujtabajunaid51@gmail.com>
2024-01-05 12:44:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var text = document.querySelector("div"),
+input = document.querySelector("input");
+
+input.addEventListener("input" , () =>{
+    text.style.color = input.value;
+    text.style.fontSize = input.value;
+})
